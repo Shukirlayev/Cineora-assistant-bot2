f1e2d5087f0bb6d736b7a368dda8c71453628c33
Sarvar Shukirlayev <Shukirlayevs@gmail.com>
2026-06-03 06:21:58 +0500

    Create index.js

---
+++ src/config/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+if (!process.env.BOT_TOKEN || !process.env.OWNER_ID) {
+    console.error("❌ XAVFSIZLIK XATOSI: BOT_TOKEN yoki OWNER_ID topilmadi!");
+    process.exit(1);
+}
+
+module.exports = {
+    BOT_TOKEN: process.env.BOT_TOKEN,
+    OWNER_ID: String(process.env.OWNER_ID),
+    TELEGRAM_CHANNEL_ID: process.env.TELEGRAM_CHANNEL_ID,
+    PORT: process.env.PORT || 3000
+};
